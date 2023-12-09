602bc2847f3810e6cc6d0a8b688c95fb0e96058a
aleksandrapopova <avtikhomirova@gmail.com>
2023-12-08 16:43:42 -0800

    Implement Initial API Testing Framework for TeamCity 1st iteration
    1. Framework Structure: Implemented a comprehensive testing framework covering all essential components.
    
    2. Project Creation Tests:
    Basic and advanced scenarios including maximum allowed name and ID.
    Child project creation.
    Boundary value analysis.
    Negative tests for invalid names, IDs, parent projects, and duplicate projects.
    
    3. Build Configuration Tests:
    Standard creation process and advanced scenarios with maximum allowed name and ID.
    Boundary value analysis.
    Negative tests for invalid names, IDs, projects, and duplicate build configurations.

---
+++ src/test/java/com/example/teamcity/api/ProjectCreateTest.java
@@ -0,0 +1,169 @@
+package com.example.teamcity.api;
+
+import com.example.teamcity.api.enums.Role;
+import com.example.teamcity.api.generators.RandomData;
+import com.example.teamcity.api.generators.TestDataGenerator;
+import com.example.teamcity.api.requests.checked.CheckedProject;
+import com.example.teamcity.api.requests.unchecked.UncheckedProject;
+import com.example.teamcity.api.spec.Specifications;
+import org.apache.http.HttpStatus;
+import org.hamcrest.Matchers;
+import org.testng.annotations.Test;
+
+public class ProjectCreateTest extends BaseApiTest {
+
+    @Test
+    public void basicProjectCreationProjectAdmin() {
+        var testData = testDataStorage.addTestData();
+        testData.getUser().setRoles(TestDataGenerator.generateRoles(Role.PROJECT_ADMIN, "g"));
+        checkedWithSuperUser.getUserRequest()
+                .create(testData.getUser());
+
+        var project = new CheckedProject(Specifications.getSpec()
+                .authSpec(testData.getUser()))
+                .create(testData.getProject());
+
+        softy.assertThat(project.getId()).isEqualTo(testData.getProject().getId());
+    }
+
+    @Test
+    public void positiveProjectCreationMaxAllowedName() {
+        var testData = testDataStorage.addTestData();
+        checkedWithSuperUser.getUserRequest()
+                .create(testData.getUser());
+
+        var projectDescription = testData.getProject();
+        projectDescription.setName(RandomData.getString(75));
+
+        var project = new CheckedProject(Specifications.getSpec()
+                .authSpec(testData.getUser()))
+                .create(projectDescription);
+
+        softy.assertThat(project.getId()).isEqualTo(projectDescription.getId());
+        softy.assertThat(project.getName()).isEqualTo(projectDescription.getName());
+    }
+
+    @Test
+    public void positiveProjectCreationMaxAllowedId() {
+        var testData = testDataStorage.addTestData();
+        checkedWithSuperUser.getUserRequest()
+                .create(testData.getUser());
+
+        var projectDescription = testData.getProject();
+        projectDescription.setId(RandomData.getString(220));
+
+        var project = new CheckedProject(Specifications.getSpec()
+                .authSpec(testData.getUser()))
+                .create(projectDescription);
+
+        softy.assertThat(project.getId()).isEqualTo(projectDescription.getId());
+    }
+
+    @Test
+    public void positiveProjectCreationUnderParentProject() {
+        var parentTestData = testDataStorage.addTestData();
+        checkedWithSuperUser.getUserRequest()
+                .create(parentTestData.getUser());
+
+        var parentProject = new CheckedProject(Specifications.getSpec()
+                .authSpec(parentTestData.getUser()))
+                .create(parentTestData.getProject());
+
+        var childTestData = testDataStorage.addTestData();
+        var childProjectDescription = childTestData.getProject();
+        childProjectDescription.setParentProject(parentProject);
+
+        var childPproject = new CheckedProject(Specifications.getSpec()
+                .authSpec(parentTestData.getUser()))
+                .create(childProjectDescription);
+
+        softy.assertThat(parentProject.getId()).isEqualTo(parentProject.getId());
+        softy.assertThat(childPproject.getId()).isEqualTo(childProjectDescription.getId());
+    }
+
+    @Test
+    //Failing test.Expected status code <400> but was <500>.
+    public void negativeProjectCreationBoundaryValueId() {
+        var testData = testDataStorage.addTestData();
+        checkedWithSuperUser.getUserRequest()
+                .create(testData.getUser());
+
+        var projectDescription = testData.getProject();
+        projectDescription.setId(RandomData.getString(221));
+
+        new UncheckedProject(Specifications.getSpec()
+                .authSpec(testData.getUser()))
+                .create(projectDescription)
+                .then().assertThat().statusCode(HttpStatus.SC_BAD_REQUEST);
+    }
+
+    @Test
+    public void negativeProjectCreationEmptyName() {
+        var testData = testDataStorage.addTestData();
+        testData.getUser();
+        checkedWithSuperUser.getUserRequest()
+                .create(testData.getUser());
+
+        var projectDescription = testData.getProject();
+        projectDescription.setName("");
+
+        new UncheckedProject(Specifications.getSpec()
+                .authSpec(testData.getUser()))
+                .create(projectDescription)
+                .then().assertThat().statusCode(HttpStatus.SC_BAD_REQUEST)
+                .body(Matchers.containsString(" Project name cannot be empty"));
+    }
+
+    @Test
+    public void negativeProjectCreationWithoutId() {
+        var testData = testDataStorage.addTestData();
+        testData.getUser();
+        checkedWithSuperUser.getUserRequest()
+                .create(testData.getUser());
+
+        var projectDescription = testData.getProject();
+        projectDescription.setId("");
+
+        new UncheckedProject(Specifications.getSpec()
+                .authSpec(testData.getUser()))
+                .create(projectDescription)
+                .then().assertThat().statusCode(HttpStatus.SC_INTERNAL_SERVER_ERROR)
+                .body(Matchers.containsString("Project ID must not be empty."));
+    }
+
+    @Test
+    //Failing test.Expected status code <500> but was <200>.
+    public void negativeProjectCreationInvalidParentProjectId() {
+        var testData = testDataStorage.addTestData();
+        testData.getUser();
+        checkedWithSuperUser.getUserRequest()
+                .create(testData.getUser());
+
+        var projectDescription = testData.getProject();
+        projectDescription.setParentProject(null);
+
+        new UncheckedProject(Specifications.getSpec()
+                .authSpec(testData.getUser()))
+                .create(projectDescription)
+                .then().assertThat().statusCode(HttpStatus.SC_INTERNAL_SERVER_ERROR);
+    }
+
+    @Test
+    public void negativeProjectCreationDuplicateId() {
+        var testData = testDataStorage.addTestData();
+        checkedWithSuperUser.getUserRequest()
+                .create(testData.getUser());
+
+        new CheckedProject(Specifications.getSpec()
+                .authSpec(testData.getUser()))
+                .create(testData.getProject());
+
+        new UncheckedProject(Specifications.getSpec()
+                .authSpec(testData.getUser()))
+                .create(testData.getProject())
+                .then().assertThat().statusCode(HttpStatus.SC_BAD_REQUEST)
+                .body(Matchers.containsString("Project with this name already exists"));
+
+    }
+
+}
